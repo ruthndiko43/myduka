95948e84a29475bd65c5130e74d6b791caa7e882
ruthndiko43 <ruthndiko43@gmail.com>
2026-06-10 16:50:56 +0300

    template_inheritance

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-from flask import Flask
+from flask import Flask,render_template
 
 #flask instance
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello World"
+@app.route('/index')
+def index():
+    return render_template("index.html")
 
 
-@app.route('/products')
+@app.route('/product')
 def products():
-    return "product section"
+    return render_template("product.html")
+
+@app.route('/sales')
+def sales():
+    return render_template("sales.html")
+
+@app.route('/stock')
+def stock():
+    return render_template("stock.html")
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
 
 app.run()
